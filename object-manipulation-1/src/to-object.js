75697789ecd2cc72591bebd47e18ad18bef8a26f
Keenan Ng <keenandng@gmail.com>
2020-10-05 17:57:54 -0700

    object-manipulation-1 | Keenan Ng

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+
+// const toObject = keyValuePair => {
+//   const myObj = {};
+//   myObj[keyValuePair[0]] = keyValuePair[1];
+//   return myObj;
+// };
+
+// es6 alternative
+const toObject = keyValuePair => {
+  return { [keyValuePair[0]]: keyValuePair[1] };
+};
