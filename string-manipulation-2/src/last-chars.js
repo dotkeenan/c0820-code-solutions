586280806fffbbba7ef14a89a1bfd66992582288
Keenan Ng <keenandng@gmail.com>
2020-10-06 12:40:11 -0700

    string-manipulation-2 | Keenan Ng

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,2 @@
 /* eslint-disable no-unused-vars */
+const lastChars = (length, string) => string.slice(-length);
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+
+// Works but seems wrong since I turned it into an array
+// const swapChars = (firstIndex, secondIndex, string) => {
+//   const newArr = string.split('');
+//   const tempValue = newArr[firstIndex];
+//   newArr[firstIndex] = newArr[secondIndex];
+//   newArr[secondIndex] = tempValue;
+//   return newArr.join('');
+// };
+
+const swapChars = (firstIndex, secondIndex, string) => {
+  let newStr = '';
+  for (let i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      newStr += string[secondIndex];
+    } else if (i === secondIndex) {
+      newStr += string[firstIndex];
+    } else newStr += string[i];
+  }
+  return newStr;
+};
