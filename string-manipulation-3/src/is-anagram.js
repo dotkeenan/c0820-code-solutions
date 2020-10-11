59561c4f3629b5f96db978537658c635d4403d49
Keenan Ng <keenandng@gmail.com>
2020-10-11 16:50:06 -0700

    completed anagram | relied heavily on methods

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,7 @@
 /* eslint-disable no-unused-vars */
+const isAnagram = (firstString, secondString) => {
+  // relied HEAVILY on methods.  Possibly attempt later with no methods?
+  const sortFirst = firstString.split('').sort().join('').trim();
+  const sortSecond = secondString.split('').sort().join('').trim();
+  return (sortFirst === sortSecond);
+};
