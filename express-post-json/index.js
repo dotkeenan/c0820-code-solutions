48eb478b7130f95692d92e6ae744640697fbe0cf
Keenan Ng <keenandng@gmail.com>
2020-10-14 15:01:41 -0700

    express-post-json | completed

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,31 +1,46 @@
 /* eslint-disable no-console */
 const express = require('express');
 const app = express();
-// const path = require('path');
 
 const grades = [];
 let nextId = 1;
 
-const getJson = app.get('/api/grades', (req, res, next) => {
+app.get('/api/grades', (req, res) => {
   res.json(grades);
 });
 
-const parseJson = express.json(getJson);
+app.use(express.json());
 
-app.post('/api/grades', parseJson, (req, res) => {
+app.post('/api/grades', (req, res) => {
   req.body.id = nextId;
   grades.push(req.body);
   res.status(201).send(req.body);
   nextId++;
 });
 
-// app.post('/api/grades');
+app.listen(3000, () => console.log('Listening on port 3000'));
 
-// app.post('/api/grades', (req, res, next) => {
-//   console.log('hello');
-//   req.id = nextId;
-//   grades.push(req);
+// Other way in which I assigned all the app methods to a const and passed them
+// as args in other methods.
+// /* eslint-disable no-console */
+// const express = require('express');
+// const app = express();
+
+// const grades = [];
+// let nextId = 1;
+
+// const getJson = app.get('/api/grades', (req, res) => {
+//   res.json(grades);
+// });
+
+// const parseJson = express.json(getJson);
+// app.use(parseJson)
+
+// app.post('/api/grades', parseJson, (req, res) => {
+//   req.body.id = nextId;
+//   grades.push(req.body);
+//   res.status(201).send(req.body);
 //   nextId++;
 // });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+// app.listen(3000, () => console.log('Listening on port 3000'));
