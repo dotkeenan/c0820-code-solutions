ebd8b8bf27213d97f33d15c1ddbb56477a720f17
Keenan Ng <keenandng@gmail.com>
2020-10-14 17:18:46 -0700

    express-post-json | added solution code for future studying

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,24 +1,30 @@
 /* eslint-disable no-console */
-const express = require('express');
-const app = express();
 
+// revised to reflect solution for future studying
+const express = require('express');
 const grades = [];
 let nextId = 1;
 
-app.get('/api/grades', (req, res) => {
+const app = express();
+
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.get('/api/grades', (req, res, next) => {
   res.json(grades);
 });
 
-app.use(express.json());
-
-app.post('/api/grades', (req, res) => {
-  req.body.id = nextId;
-  grades.push(req.body);
-  res.status(201).send(req.body);
-  nextId++;
+app.post('/api/grades', (req, res, next) => {
+  const newGrade = req.body;
+  newGrade.id = nextId++;
+  grades.push(newGrade);
+  res.status(201).json(newGrade);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
 
 // Other way in which I assigned all the app methods to a const and passed them
 // as args in other methods.
