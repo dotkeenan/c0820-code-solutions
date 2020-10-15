4666d32eab96849ea7a472069150482c3caafd15
Keenan Ng <keenandng@gmail.com>
2020-10-14 17:13:46 -0700

    Work in progress

---
+++ express-delete/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const myArr = [
+  {
+    id: 1,
+    name: 'Keenan',
+    course: 'JavaScript',
+    grade: 99
+  },
+  {
+    id: 1,
+    name: 'Keenan',
+    course: 'JavaScript',
+    grade: 99
+  }
+];
