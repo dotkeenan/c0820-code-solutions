df1fd1749afc79d43dad03745170f5f412198bfc
Keenan Ng <keenandng@gmail.com>
2020-10-16 00:22:56 -0700

    completed feature 2 | User can get a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -3,13 +3,40 @@ const express = require('express');
 const app = express();
 const dataJson = require('./data.json');
 const notes = dataJson.notes;
-const notesValues = Object.values(notes);
+// const notes = dataJson.notes;
+// const notesValues = Object.values(notes);
 // const path = require('path');
 // const publicPath = path.join(__dirname, 'public');
 // const middlewareFunc = express.static(absolutePath);
 
 const jsonMiddleware = express.json();
 app.use(jsonMiddleware);
+
+app.get('/api/notes', (req, res, next) => {
+  const notesArr = [];
+  for (const props in dataJson.notes) {
+    notesArr.push(dataJson.notes[props]);
+  }
+  res.status(200).json(notesArr);
+});
+
+app.get('/api/notes/:id', (req, res, next) => {
+  const inputId = Number(req.params.id, 10);
+  // not sure how to rule out cases for when :id is like 10hhh
+  // Number() might be the way. since 10hhh returns NaN.
+  if (!Number.isInteger(inputId) || inputId <= 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+    return;
+  }
+  // if (!notes[inputId]) {
+  if (typeof notes[inputId] === 'undefined') {
+    res.status(404).json({ error: `note of id:${inputId} cannot be found` });
+    return;
+  }
+  res.status(200).json(notes[inputId]);
+});
+
+/*  almost working code
 
 app.get('/api/notes', (req, res, next) => {
   res.json(Object.values(notes));
@@ -30,7 +57,7 @@ app.get('/api/notes/:id', (req, res, next) => {
     console.log('parsedId:', parsedId);
     res.status(200).json(notesValues[noteIndex]);
   }
-});
+}); */
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
