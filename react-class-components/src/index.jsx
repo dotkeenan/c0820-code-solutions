dec0b3c0893a2693ba3caa7825ad572af234ea58
Keenan Ng <keenandng@gmail.com>
2020-10-21 11:35:41 -0700

    react-class-components | Keenan Ng

---
+++ react-class-components/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// function CustomButton(props) {
+//   return (
+//     <button>{props.text}</button>
+//   );
+// }
+
+class CustomButton extends React.Component {
+  render() {
+    return <button> { this.props.text }</button >;
+  }
+}
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+ReactDOM.render(
+  element,
+  document.querySelector('#root')
+);
