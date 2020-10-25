3c453fcbfcd7f24108ce045d0d204c65627a9ca1
Keenan Ng <keenandng@gmail.com>
2020-10-25 16:46:45 -0700

    array-manipulation-2 | Keenan Ng

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,47 +1,24 @@
 /* eslint-disable no-unused-vars */
-// const chunk = (array, size) => {
-//   let n = 0;
-//   const newArr = [];
-//   for (let i = 0; i < array.length; i++) {
-//     if (i < size) {
-//       const tempArr = array[i];
-//       newArr.push(tempArr);
-//     } else {
-//       n++;
-//     }
-//   }
-//   console.log(newArr);
-//   return newArr;
-// };
-
-// const chunk = (array, size) => {
-//   array.reduce((resultArray, item, index) => {
-//     const chunkIndex = Math.floor(index / size);
-
-//     if (!resultArray[chunkIndex]) {
-//       resultArray[chunkIndex] = [];
-//     }
-//     resultArray[chunkIndex].push(item);
-//     console.log(resultArray);
-//     return resultArray;
-
-//   }, []);
-
-// };
-
 const chunk = (array, size) => {
-  const newArr = [];
-  for (let i = 0; i < array.length; i++) {
-    newArr.push(array.slice(i, size));
-    i += size;
+  const chunkedArr = [];
+  for (const elem of array) {
+    const last = chunkedArr[chunkedArr.length - 1];
+    if (!last || last.length === size) {
+      chunkedArr.push([elem]);
+    } else {
+      last.push(elem);
+    }
   }
-  // console.log(newArr);
+  return chunkedArr;
 };
 
-/*
-Need to iterate over the full array.
-Need to divide the array into smaller arrays that are the length of 'size'
+// previous attempt that should work.
 
-need to push the individual small arrays into a new array and return that array.
-
-*/
+/* const chunk = (array, size) => {
+  const newArr = [];
+  for (let i = 0; i < array.length; i += size) {
+    newArr.push([array.slice(i, i + size)]);
+  }
+  console.log(newArr);
+  return newArr;
+}; */
