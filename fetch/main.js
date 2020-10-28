5d0bd658c63b4fb225f79efb187a809f1699f6ec
Keenan Ng <keenandng@gmail.com>
2020-10-28 11:33:08 -0700

    added extra pokemon fetch request on 'fetch' exercise

--- fetch/main.js
+++ fetch/main.js
@@ -5,3 +5,9 @@ const response = fetch('https://jsonplaceholder.typicode.com/users')
   // .catch(error => console.error(error.message));
 
 console.log('promise info:', response);
+
+const pokemon = fetch('https://pokeapi.co/api/v2/pokemon/gengar')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+console.log('promise info2:', pokemon);
